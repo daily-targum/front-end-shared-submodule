8d5fc95cc983b32ad86982f2b4933175b5e9b0a8
Christian Juth <cjuth2@gmail.com>
2020-05-13 16:40:03 -0400

    refactor: change secrets

--- src/secrets.native.ts
+++
@@ -1,5 +0,0 @@
-import Constants from 'expo-constants';
-
-export const AWS_APPSYNC_URL = Constants.manifest.extra.AWS_APPSYNC_URL;
-export const AWS_APPSYNC_REGION = Constants.manifest.extra.AWS_APPSYNC_REGION;
-export const AWS_APPSYNC_API_KEY = Constants.manifest.extra.AWS_APPSYNC_API_KEY;
--- src/secrets.ts
+++ src/secrets.ts
@@ -1,3 +1,5 @@
-export const AWS_APPSYNC_URL = '';
-export const AWS_APPSYNC_REGION = '';
-export const AWS_APPSYNC_API_KEY = '';
+import Constants from 'expo-constants';
+
+export const AWS_APPSYNC_URL = Constants.manifest.extra.AWS_APPSYNC_URL;
+export const AWS_APPSYNC_REGION = Constants.manifest.extra.AWS_APPSYNC_REGION;
+export const AWS_APPSYNC_API_KEY = Constants.manifest.extra.AWS_APPSYNC_API_KEY;
