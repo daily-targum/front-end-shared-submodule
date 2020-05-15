39a069f1f09f5803e89e83fcae3e7f772721b200
Christian Juth <cjuth2@gmail.com>
2020-05-15 14:11:13 -0400

    ci: setup github actions

--- src/secrets.ts
+++ src/secrets.ts
@@ -1,5 +1,5 @@
-import Constants from 'expo-constants';
+const Constants: any = require('expo-constants');
 
-export const AWS_APPSYNC_URL = Constants.manifest.extra.AWS_APPSYNC_URL;
-export const AWS_APPSYNC_REGION = Constants.manifest.extra.AWS_APPSYNC_REGION;
-export const AWS_APPSYNC_API_KEY = Constants.manifest.extra.AWS_APPSYNC_API_KEY;
+export const AWS_APPSYNC_URL = Constants?.manifest?.extra?.AWS_APPSYNC_URL;
+export const AWS_APPSYNC_REGION = Constants?.manifest?.extra?.AWS_APPSYNC_REGION;
+export const AWS_APPSYNC_API_KEY = Constants?.manifest?.extra?.AWS_APPSYNC_API_KEY;
