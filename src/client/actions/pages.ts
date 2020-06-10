1367a60348d6eaab5d4de6c3917612dacdbd3721
Christian Juth <cjuth2@gmail.com>
2020-06-10 19:02:10 -0400

    feat: add page query

---
+++ src/client/actions/pages.ts
@@ -0,0 +1,38 @@
+import { Document } from '@contentful/rich-text-types';
+
+import { client } from '../client';
+import gql from 'graphql-tag';
+
+export interface GetPage {
+  id: string;
+  title: string;
+  published: string;
+  formattedSlug: string;
+  content: string;
+}
+
+export async function getPage({
+  slug
+}: {
+  slug: string
+}): Promise<GetPage> {
+  const res: any = await client.query({
+    query: gql`
+      query getPageBySlug($slug: String!) {
+        getPageBySlug(slug: $slug){
+          id
+          title
+          published
+          slug
+          formattedSlug
+          content
+        }
+      }
+    `,
+    fetchPolicy: 'no-cache',
+    variables: {
+      slug
+    }
+  });
+  return res.data.getPageBySlug[0];
+}
